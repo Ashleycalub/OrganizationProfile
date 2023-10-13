56b15a658e04e2076c04643bf122da3012d9293d
Reyes.275973 <Reyes.275973@LAB-A-PC11>
2023-10-13 18:16:38 +0800

    Add project files.

---
+++ OrganizationProfile/frmConfirmation.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace OrganizationProfile
+{
+    partial class frmConfirmation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label1 = new Label();
+            label8 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            studentno = new Label();
+            name = new Label();
+            program = new Label();
+            gender = new Label();
+            birthday = new Label();
+            age = new Label();
+            contactno = new Label();
+            frmRegistration = new Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(23, 32);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.No;
+            label2.Size = new Size(104, 21);
+            label2.TabIndex = 43;
+            label2.Text = "Student No.:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(70, 73);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(57, 21);
+            label1.TabIndex = 44;
+            label1.Text = "Name:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(49, 114);
+            label8.Name = "label8";
+            label8.RightToLeft = RightToLeft.No;
+            label8.Size = new Size(78, 21);
+            label8.TabIndex = 55;
+            label8.Text = "Program:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(83, 157);
+            label4.Name = "label4";
+            label4.RightToLeft = RightToLeft.No;
+            label4.Size = new Size(44, 21);
+            label4.TabIndex = 56;
+            label4.Text = "Age:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(50, 199);
+            label5.Name = "label5";
+            label5.RightToLeft = RightToLeft.No;
+            label5.Size = new Size(77, 21);
+            label5.TabIndex = 57;
+            label5.Text = "Birthday:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(58, 242);
+            label9.Name = "label9";
+            label9.RightToLeft = RightToLeft.No;
+            label9.Size = new Size(69, 21);
+            label9.TabIndex = 58;
+            label9.Text = "Gender:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(26, 282);
+            label10.Name = "label10";
+            label10.RightToLeft = RightToLeft.No;
+            label10.Size = new Size(101, 21);
+            label10.TabIndex = 59;
+            label10.Text = "Contact No:";
+            // 
+            // studentno
+            // 
+            studentno.AutoSize = true;
+            studentno.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            studentno.Location = new Point(133, 32);
+            studentno.Name = "studentno";
+            studentno.RightToLeft = RightToLeft.No;
+            studentno.Size = new Size(0, 21);
+            studentno.TabIndex = 60;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            name.Location = new Point(133, 73);
+            name.Name = "name";
+            name.RightToLeft = RightToLeft.No;
+            name.Size = new Size(0, 21);
+            name.TabIndex = 61;
+            // 
+            // program
+            // 
+            program.AutoSize = true;
+            program.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            program.Location = new Point(133, 114);
+            program.Name = "program";
+            program.RightToLeft = RightToLeft.No;
+            program.Size = new Size(0, 21);
+            program.TabIndex = 62;
+            // 
+            // gender
+            // 
+            gender.AutoSize = true;
+            gender.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            gender.Location = new Point(133, 242);
+            gender.Name = "gender";
+            gender.RightToLeft = RightToLeft.No;
+            gender.Size = new Size(0, 21);
+            gender.TabIndex = 65;
+            // 
+            // birthday
+            // 
+            birthday.AutoSize = true;
+            birthday.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            birthday.Location = new Point(133, 199);
+            birthday.Name = "birthday";
+            birthday.RightToLeft = RightToLeft.No;
+            birthday.Size = new Size(0, 21);
+            birthday.TabIndex = 64;
+            // 
+            // age
+            // 
+            age.AutoSize = true;
+            age.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            age.Location = new Point(133, 157);
+            age.Name = "age";
+            age.RightToLeft = RightToLeft.No;
+            age.Size = new Size(0, 21);
+            age.TabIndex = 63;
+            // 
+            // contactno
+            // 
+            contactno.AutoSize = true;
+            contactno.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            contactno.Location = new Point(133, 282);
+            contactno.Name = "contactno";
+            contactno.RightToLeft = RightToLeft.No;
+            contactno.Size = new Size(0, 21);
+            contactno.TabIndex = 66;
+            // 
+            // frmRegistration
+            // 
+            frmRegistration.BackColor = Color.LightCoral;
+            frmRegistration.FlatStyle = FlatStyle.Flat;
+            frmRegistration.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            frmRegistration.Location = new Point(133, 336);
+            frmRegistration.Name = "frmRegistration";
+            frmRegistration.Size = new Size(112, 34);
+            frmRegistration.TabIndex = 67;
+            frmRegistration.Text = "Submit";
+            frmRegistration.UseVisualStyleBackColor = false;
+            // 
+            // frmConfirmation
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(373, 385);
+            Controls.Add(frmRegistration);
+            Controls.Add(contactno);
+            Controls.Add(gender);
+            Controls.Add(birthday);
+            Controls.Add(age);
+            Controls.Add(program);
+            Controls.Add(name);
+            Controls.Add(studentno);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label8);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Name = "frmConfirmation";
+            Text = "Confirmation";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private Label label8;
+        private Label label4;
+        private Label label5;
+        private Label label9;
+        private Label label10;
+        private Label studentno;
+        private Label name;
+        private Label program;
+        private Label gender;
+        private Label birthday;
+        private Label age;
+        private Label contactno;
+        private Button frmRegistration;
+    }
+}
---
+++ OrganizationProfile/frmRegistration.cs
@@ -0,0 +1,10 @@
+namespace OrganizationProfile
+{
+    public partial class frmRegistration : Form
+    {
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
